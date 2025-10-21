398490b3a32d958b9d789fb2109a5a2b656f58cd
Yu Heng Seow <seowyh98@gmail.com>
2025-10-21 22:26:30 +1100

    final touch

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const stateDropdown = document.getElementById('stateFilter');
-let mapView, barView;
+let mapView, donutView;
 
 // Remove any embedded dropdowns from Vega specs
 function removeInternalDropdown(spec) {
@@ -14,30 +14,30 @@ function removeInternalDropdown(spec) {
 function applyFilter(stateValue) {
   const selected = stateValue || null;
   if (mapView) mapView.signal('state_selection', selected).run();
-  if (barView) barView.signal('state_selection', selected).run();
+  if (donutView) donutView.signal('state_selection', selected).run();
 }
 
 // Load charts
 Promise.all([
   fetch('js/map.json').then(r => r.json()),
-  fetch('js/bar_chart.json').then(r => r.json()),
+  fetch('js/donut_chart.json').then(r => r.json()),
   fetch('js/funding_source_side_by_side.json').then(r => r.json()),
   fetch('js/bubble_chart.json').then(r => r.json())
 ])
-  .then(([mapSpec, barSpec, fundingSpec, bubbleSpec]) => {
+  .then(([mapSpec, donutSpec, fundingSpec, bubbleSpec]) => {
     const cleanedMapSpec = removeInternalDropdown(mapSpec);
-    const cleanedBarSpec = removeInternalDropdown(barSpec);
+    const cleanedDonutSpec = removeInternalDropdown(donutSpec);
 
     return Promise.all([
       vegaEmbed('#hospital_map', cleanedMapSpec, {actions: false}),
-      vegaEmbed('#hospital_bar', cleanedBarSpec, {actions: false}),
+      vegaEmbed('#hospital_bar', cleanedDonutSpec, {actions: false}),
       vegaEmbed('#funding_bar', fundingSpec, {actions: false}),
       vegaEmbed('#bubble_chart', bubbleSpec, {actions: false})
     ]);
   })
-  .then(([mapRes, barRes]) => {
+  .then(([mapRes, donutRes]) => {
     mapView = mapRes.view;
-    barView = barRes.view;
+    donutView = donutRes.view;
     applyFilter(stateDropdown.value);
   })
   .catch(console.error);
